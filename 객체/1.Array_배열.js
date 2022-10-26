34945c6309775670ac0989e046084887226993d3
MooseWithBear <107022571+MooseWithBear@users.noreply.github.com>
2022-10-26 11:37:45 +0900

    [update] JS 드디어 공부시작

---
+++ 객체/1.Array_배열.js
@@ -0,0 +1,14 @@
+//! 변수 선언 var, let, const
+const apple = '사과';
+//! 배열 사용
+const fruits = ['사과','오렌지','배','수박'];
+//! 배열의 index (자리수)
+console.log(fruits[1]); //* 오렌지
+//! 이차원배열 (배열안에 배열)
+const arrayOfArray = [[1,2,3],[4,5]]
+console.log(arrayOfArray[0]); //* [1,2,3]
+//! 배열에 값을 미리 넣지 않아도 가능, 배열의 형식이 달라도 가능
+const a =10; const b =20;
+const dayangArray = [a,b,30];
+console.log(dayangArray[1]) //* 20
+//
