69b1cd8296c538862de672516f1ae18fc3905995
MooseWithBear <moosewithbear@gmail.com>
2023-03-22 21:10:13 +0900

    20230322

--- 낙서.js
+++ 낙서.js
@@ -1,3 +1,1 @@
-let arr = [1,2,3,4,5,6,1,3]
-console.log(new Set(arr).size)
-console.log(arr)
+console.log(toString(1e1));
--- 풀이장.js
+++
@@ -1,7 +0,0 @@
-const babbling = ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"];
-const nephew = ["aya", "ye", "woo", "ma"];
-const solution = (babbling) => {
-
-}
-
-solution(babbling);
