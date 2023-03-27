6a94bea11c61451448d0d332c27dbf71e65449f1
MooseWithBear <moosewithbear@gmail.com>
2023-03-27 19:39:59 +0900

    풀이중

---
+++ CodingTest/풀이/Lv0_치킨 쿠폰.js
@@ -0,0 +1,10 @@
+//! 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120884
+//! 난이도 : Lv 0, 정답률 72% (2023/03/27)
+//! 소요시간 : 18:31
+//! 풀이방법 : 
+//! 개선할점 : 
+            // 1. [...new Set(array)], clone.slice().array.sort((a,b)=>a-b) 를 잊지말자
+            // 2. map, foreach, reduce 함수를 활용하는 방법을 좀 더 공부해야한다.
+            // 3. Map 객체를 새롭게 알게되었다. (키-값)을 함께 가질 수 있으면서 사용할 수 있는 내장함수들이 여러개 있다. 공부하면 활용할 수 있는 곳들이 많을 것 같다.
+            // 4. 
+
