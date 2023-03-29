f4d392a73a2f7c48f567dc196c1ce98f15a7a4a9
MooseWithBear <moosewithbear@gmail.com>
2023-03-29 21:19:07 +0900

    20230329

--- 낙서.js
+++ 낙서.js
@@ -1,8 +1,8 @@
 // console.log(totring(1e1));
 
 
-const chicken = 1081
-const service = 5
+const chicken = 100
+const service = 10
 function solution(chicken) {
     var answer = parseInt((chicken-1) / (service-1));
     console.log(answer);
