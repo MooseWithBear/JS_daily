54a0bedf46f044e58bff4383fe6c6f9673586804
MooseWithBear <moosewithbear@gmail.com>
2023-03-30 21:17:48 +0900

    풀이중

---
+++ CodingTest/풀이/Lv0_특이한 정렬.js
@@ -0,0 +1,6 @@
+//! 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120880
+//! 난이도 : Lv 0, 정답률 70% (2023/03/30)
+//! 소요시간 : 2:42
+//! 풀이방법 : 
+//! 개선할점 : 
+    // 1. 
---
+++ 풀이장.js
@@ -0,0 +1,40 @@
+const numlist = [70,20,36,47,40,6,10,100]
+const n = 30;
+let result = []
+const solution = (n, numlist) => {
+    let sorting = numlist.slice()
+if(numlist.indexOf(n) === -1) {
+    sorting.push(n)
+}
+    sorting.sort((a,b)=>a-b); // [1,2,3,4,5,6]
+while(sorting.length > 1){
+    const standard = sorting[sorting.indexOf(n)]
+    const forward = sorting[sorting.indexOf(n)-1]
+    const back = sorting[sorting.indexOf(n)+1]
+    // console.log(forward, standard, back)
+    if(Math.abs(standard-forward) === Math.abs(standard-back) || 
+    Math.abs(standard-forward) > Math.abs(standard-back)) {
+        result.push(back);
+        sorting.splice(sorting.indexOf(n)+1,1)
+    } else {
+        result.push(forward);
+        sorting.splice(sorting.indexOf(n)-1,1)
+    }
+
+
+}
+
+
+
+
+
+if(numlist.indexOf(n) === -1) {
+    console.log('result: ', result);
+    return
+}else {
+    result.unshift(n)
+
+}
+}
+
+solution(n, numlist)
