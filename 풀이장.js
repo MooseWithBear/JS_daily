1455d63cbdb62d8876fa95c3f2a48e7e6aab2b16
MooseWithBear <moosewithbear@gmail.com>
2023-04-04 22:04:05 +0900

    20230404

--- 풀이장.js
+++ 풀이장.js
@@ -1,4 +1,4 @@
-const numlist = [70,20,36,47,40,6,10,100]
+const numlist = [36, 40, 20, 47, 10, 6, 7000, 10000]
 const n = 30;
 let result = []
 const solution = (n, numlist) => {
@@ -7,12 +7,18 @@ if(numlist.indexOf(n) === -1) {
     sorting.push(n)
 }
     sorting.sort((a,b)=>a-b); // [1,2,3,4,5,6]
+    console.log(sorting)
 while(sorting.length > 1){
     const standard = sorting[sorting.indexOf(n)]
     const forward = sorting[sorting.indexOf(n)-1]
     const back = sorting[sorting.indexOf(n)+1]
     // console.log(forward, standard, back)
-    if(Math.abs(standard-forward) === Math.abs(standard-back) || 
+    if(forward === undefined) {
+        result.push(back);
+        sorting.splice(sorting.indexOf(n)+1,1);
+    }
+    
+    else if(Math.abs(standard-forward) === Math.abs(standard-back) || 
     Math.abs(standard-forward) > Math.abs(standard-back)) {
         result.push(back);
         sorting.splice(sorting.indexOf(n)+1,1)
@@ -20,17 +26,9 @@ while(sorting.length > 1){
         result.push(forward);
         sorting.splice(sorting.indexOf(n)-1,1)
     }
-
-
+    
+    console.log('sorting: ', sorting);
 }
-
-
-
-
-
-
-
-
 
 if(numlist.indexOf(n) === -1) {
     console.log('result: ', result);
