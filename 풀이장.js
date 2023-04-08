af9aadfa385eaba7a077c8b9f0a5e00c98de792c
MooseWithBear <moosewithbear@gmail.com>
2023-04-08 20:45:08 +0900

    20230408

--- 풀이장.js
+++ 풀이장.js
@@ -1,42 +1,23 @@
-const numlist = [36, 40, 20, 47, 10, 6, 7000, 10000]
-const n = 30;
-let result = []
-const solution = (n, numlist) => {
-    let sorting = numlist.slice()
-if(numlist.indexOf(n) === -1) {
-    sorting.push(n)
-}
-    sorting.sort((a,b)=>a-b); // [1,2,3,4,5,6]
-    console.log(sorting)
-while(sorting.length > 1){
-    const standard = sorting[sorting.indexOf(n)]
-    const forward = sorting[sorting.indexOf(n)-1]
-    const back = sorting[sorting.indexOf(n)+1]
-    // console.log(forward, standard, back)
-    if(forward === undefined) {
-        result.push(back);
-        sorting.splice(sorting.indexOf(n)+1,1);
+quiz = ["3 - -4 = -3", "5 + 6 = 11"]
+function solution(quiz) {
+ let result =[]
+    // var calc = quiz[0].split(" ").merg
+    for(i=0;i<quiz.length; i++){
+    var calc = quiz[i].replaceAll(" ", "");
+    var ind = calc.indexOf("=")
+    var indd = calc.substring(0, ind)
+    var inddd = calc.substring(ind+1)
+        console.log(indd,inddd)
+
+    if(eval(indd) == inddd) {
+        result.push("O")
+    }else{
+        result.push("X")
     }
-    
-    else if(Math.abs(standard-forward) === Math.abs(standard-back) || 
-    Math.abs(standard-forward) > Math.abs(standard-back)) {
-        result.push(back);
-        sorting.splice(sorting.indexOf(n)+1,1)
-    } else {
-        result.push(forward);
-        sorting.splice(sorting.indexOf(n)-1,1)
     }
-    
-    console.log('sorting: ', sorting);
-}
-
-if(numlist.indexOf(n) === -1) {
-    console.log('result: ', result);
-    return
-}else {
-    result.unshift(n)
+            console.log(result); 
 
 }
-}
 
-solution(n, numlist)
+
+solution(quiz)
