16f58e9f9795f853d108b33d92609edf8201f361
MooseWithBear <moosewithbear@gmail.com>
2023-04-09 21:12:18 +0900

    20230409

--- 풀이장.js
+++ 풀이장.js
@@ -20,4 +20,24 @@ function solution(quiz) {
 }
 
 
+solution(quiz)quiz = ["3 - -4 = -3", "5 + 6 = 11"]
+
+function solution(quiz) {
+  let result = []
+  
+  for(let i=0; i<quiz.length; i++){
+    let calc = quiz[i].replace(/\s+/g, '');
+    let ind = calc.indexOf("=")
+    let indd = calc.substring(0, ind).replace(/--/g, '+')
+    let inddd = calc.substring(ind+1)
+    if(eval(indd) == inddd) {
+        result.push("O")
+    }else{
+        result.push("X")
+    }
+  }
+  
+  console.log(result)
+}
+
 solution(quiz)
