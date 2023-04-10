604724ee7913daadc1f9141e93ed6e521be4edc1
MooseWithBear <moosewithbear@gmail.com>
2023-04-10 23:45:15 +0900

    lecture

--- 풀이장.js
+++ 풀이장.js
@@ -1,26 +1,4 @@
 quiz = ["3 - -4 = -3", "5 + 6 = 11"]
-function solution(quiz) {
- let result =[]
-    // var calc = quiz[0].split(" ").merg
-    for(i=0;i<quiz.length; i++){
-    var calc = quiz[i].replaceAll(" ", "");
-    var ind = calc.indexOf("=")
-    var indd = calc.substring(0, ind)
-    var inddd = calc.substring(ind+1)
-        console.log(indd,inddd)
-
-    if(eval(indd) == inddd) {
-        result.push("O")
-    }else{
-        result.push("X")
-    }
-    }
-            console.log(result); 
-
-}
-
-
-solution(quiz)quiz = ["3 - -4 = -3", "5 + 6 = 11"]
 
 function solution(quiz) {
   let result = []
